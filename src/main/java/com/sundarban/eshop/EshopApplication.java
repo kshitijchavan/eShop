5a4bc555210689187056819169dc061b2cefe90e
kshitijchavan <kittuchavan79@gmail.com>
2024-05-27 23:34:11 +0100

    Logger

--- src/main/java/com/sundarban/eshop/EshopApplication.java
+++ src/main/java/com/sundarban/eshop/EshopApplication.java
@@ -1,13 +1,18 @@
 package com.sundarban.eshop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
+	public static final Logger log = LoggerFactory.getLogger(EshopApplication.class);
 
 	public static void main(String[] args) {
+		log.debug("In debug mode");
 		SpringApplication.run(EshopApplication.class, args);
+		log.info("Application Started Successfully");
 	}
 
 }
